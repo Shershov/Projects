93582c444e051f7e17284ae417a38309085f6da3
Eugene Shershov <Absolute@tut.by>
2020-01-29 13:08:37 +0300

    add few cards

--- FilmBox/script/script.js
+++ FilmBox/script/script.js
@@ -17,18 +17,65 @@ let films = {
         'director': 'Мартин Скорсезе',
 
     },
+    '7': {
+        'img': '../img/posters/Wasabi-1.jpeg',
+        'name': 'Васаби',
+        'year': '2001',
+        'country': 'Франция',
+        'genre': 'комедия',
+        'director': 'Жерар Кравчик',
+    },
+    '8': {
+        'img': '../img/posters/Taxi-4.jpeg',
+        'name': 'Такси-4',
+        'year': '2007',
+        'country': 'Франция',
+        'genre': 'комедия',
+        'director': 'Жерар Кравчик',
+    },
+    '9': {
+        'img': '../img/posters/Unforgiven.jpeg',
+        'name': 'Непрощенный',
+        'year': '2018',
+        'country': 'Россия',
+        'genre': 'Драма',
+        'director': 'Сарик Андреасян',
+    },
+    '10': {
+        'img': '../img/posters/Konvert.jpeg',
+        'name': 'Конверт',
+        'year': '2017',
+        'country': 'Россия',
+        'genre': 'Ужасы',
+        'director': 'Владимир Марков',
+    },
+    '11': {
+        'img': '../img/posters/Country-03.jpeg',
+        'name': 'Страна 03',
+        'year': '2015',
+        'country': 'Россия',
+        'genre': 'Комедия',
+        'director': 'Василий Сигарев',
+    },
 };
 
 let out = '';
 
 for (let key in films) {
-    out += `<img src="${films[key].img}"`;
-    out += `${films[key].name}<br>`;
-    out += `${films[key].year}<br>`;
-    out += `${films[key].country}<br>`;
-    out += `${films[key].genre}<br>`;
-    out += `${films[key].director}<br>`;
+    out += `<div class="film__card">`;
+    out += `<a class="poster" href="">`;
+    out += `<img src="${films[key].img}" alt="">`;
+    out += `<div class="film__descr">`;
+    out += `<h3>${films[key].name} </h3>`;
+    out += `<span>${films[key].year}, </span>`;
+    out += `<span>${films[key].country}, </span>`;
+    out += `<span>${films[key].genre}, </span>`;
+    out += `<span>${films[key].director}</span>`;
+    out += `</div>`;
+    out += `</a>`;
+    out += `</div>`;
 
 }
 
-document.getElementById('out').innerHTML = out;
+let div = document.getElementById('films');
+div.innerHTML = div.innerHTML + out;
