ed05c320f0b3f47eee1b6d2a0c34b64bbb64d9dd
Eugene Shershov <Absolute@tut.by>
2020-02-09 20:02:53 +0300

    guitar js

---
+++ guitar-shop/components/Shopping/Shopping.js
@@ -0,0 +1,40 @@
+class Shopping {
+    handleClear() {
+        ROOT_SHOPPING.innerHTML = '';
+    }
+
+    render() {
+        const productsStore = localStorageUtil.getProducts();
+        let htmlCatalog = '';
+        let sumlCatalog = 0;
+
+        CATALOG.forEach(({ id, name, price }) => {
+            if (productsStore.indexOf(id) !== -1) {
+                htmlCatalog += `
+               <tr>
+               <td class="shopping-element__name">⚡️ ${name}</td>
+               <td class="shopping-element__price">${price.toLocaleString()} USD</td>
+               </tr>
+               `;
+                sumlCatalog += price;
+            }
+
+        });
+
+        const html = `
+        <div class="shopping-container">
+        <div class="shopping__close" onclick="shoppingPage.handleClear();"></div>
+        <table>
+         ${htmlCatalog}
+         <tr>
+         <td class="shopping-element__name">💥 Сумма:</td>
+          <td class="shopping-element__price">${sumlCatalog.toLocaleString()} USD</td>
+         </tr>
+         </table>
+        </div>
+        `;
+        ROOT_SHOPPING.innerHTML = html;
+    }
+}
+
+const shoppingPage = new Shopping();
